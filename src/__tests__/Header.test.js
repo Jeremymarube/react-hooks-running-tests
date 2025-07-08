901e89555d3a621f8c1b8695bbc0a3c2584a7196
Jeremy Marube <jeremymarube1@gmail.com>
2025-07-08 07:17:48 +0300

    Passed all tests for Header and Article components

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -2,10 +2,15 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
+
 import Header from "../components/Header";
 
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
+
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug();
 
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
